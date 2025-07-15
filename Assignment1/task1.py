55d4812bf64b9475c47b63dbcbf8cd3560f0afdd
Dharmendra Kumar <dharmendrakumar.voona@gmail.com>
2025-07-15 12:06:32 +0530

    Assignment1 completed

---
+++ Assignment1/task1.py
@@ -0,0 +1,12 @@
+
+a = int(input('Enter the first number: '))
+b = int(input('Enter the second number: '))
+
+add = a + b
+print(f'Addition: {add}')
+subtract = a - b
+print(f'Subtraction: {subtract}')
+multiply = a * b
+print(f'Multiplication: {multiply}')
+div = a / b
+print(f'Division: {div}')
